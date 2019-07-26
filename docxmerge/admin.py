a9449712668e079d04382b61caa39ceb6fc0a593
qwlake <qwlake@gmail.com>
2019-07-26 15:56:28 +0900

    edit files

--- docxmerge/admin.py
+++ docxmerge/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ResumeInfo, Resume
+from .models import ResumeInfo, Resume, ResumeMerged
 
 admin.site.register(ResumeInfo)
 admin.site.register(Resume)
+admin.site.register(ResumeMerged)
