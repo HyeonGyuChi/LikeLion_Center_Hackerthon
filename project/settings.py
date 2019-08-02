7518f98a852d9043901437113687b7544f90f826
qwlake <qwlake@gmail.com>
2019-08-02 16:44:23 +0900

    complete loading page

--- project/settings.py
+++ project/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'docxmerge', 'static'),
 ]
 
-# # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
