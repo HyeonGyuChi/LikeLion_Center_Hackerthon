4fb0533ca925d087223bf94ffe664d2de5f9a163
qwlake <qwlake@gmail.com>
2019-08-08 17:27:01 +0900

    db는 authtools로, form 및 소셜로그인은 allauth로

--- users/urls.py
+++ users/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from authtools.views import LoginView, LogoutView
+from allauth.account.views import SignupView, LoginView, LogoutView
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-#    path('signup', views.signup, name="signup"), > account_signup
-    path('login', LoginView.as_view(), name="login"),  #>  account_login
-#    path('logout', LogoutView.as_view(), name="logout"),  > account_logout
+    path('signup', SignupView.as_view(), name="account_signup"),    # account_signup
+    path('login', LoginView.as_view(), name="account_login"),       #  account_login
+    path('logout', LogoutView.as_view(), name="account_logout"),    # account_logout
     path('mypage', views.mypage, name="mypage"),
 ]
